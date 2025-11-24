757c3ccf9fff40b922d41d360dcaf16e8e599f90
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-24 14:47:16 +0600

    Backend part almost done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-
 import ProductRoutes from "./routes/product.routes.js";
 import sliderRoutes from "./routes/slider.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
@@ -8,9 +7,15 @@ import categoryRoutes from "./routes/category.routes.js";
 
 const app= express();
 
+
+
 app.use(express.json());
 app.use(cors());
 
+
+app.get("/", (req, res) => {
+  res.send("API is working. Welcome to the backend.");
+});
 
 // API Routes
 app.use("/api/products",ProductRoutes);
